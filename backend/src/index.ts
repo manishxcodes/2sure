f0b12f91f710a556f5e49826cbdd45cdff05b41b
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-08-22 02:20:12 +0530

    chore setup server

---
+++ backend/src/index.ts
@@ -0,0 +1,40 @@
+import express, { urlencoded } from 'express';
+import session from 'express-session';
+import dotnev from 'dotenv';
+import passport from 'passport';
+import cors from 'cors';
+
+dotnev.config();
+
+const app = express();
+
+// middleware
+const corsOptions = {
+    origin: ["http://localhost:3001"],
+    credentials: true               // to supports cookies/ sessions/ headers
+}
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "100md" }));
+app.use(urlencoded({ limit: "100md", extended: true}));
+app.use(session({
+    secret: process.env.SESSION_SECRET || "SECRET-SESSION-skfafajhfjalkfjaoifh",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60, // 1 hour
+        // httpOnly: true,         // JS can’t read cookie (prevents XSS)
+        // secure: true,           // send only over HTTPS (set true in prod)
+        // sameSite: "lax"         // helps protect against CSRF
+    }
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
+
+// listen app
+const PORT = process.env.PORT || 7002;
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
