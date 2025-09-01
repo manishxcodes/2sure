bc40d315dec156604b1a4b3da97b51b2ac1036e4
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-09-02 00:50:46 +0530

    rename: types.ts to schema.ts

---
+++ frontend/src/schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+    username: z.email("Enter a valid email"),
+    password: z.string().min(8, "Password must be at least 8 characters long").max(24, "Password cannot exceed 24 characters")
+})
+
+export const registerSchema = loginSchema.extend({
+    confirmPassword: z.string(),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Password dont match",
+    path: ["confirmPassword"]
+})
--- frontend/src/types.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const userDetailsSchema = z.object({
-    username: z.email("Use a valid email"),
-    password: z.string().min(8, "Password must be at least 8 characters long").max(24, "Password cannot exceed 24 characters")
-})
-
-export type UserDetails = z.infer<typeof userDetailsSchema>;
