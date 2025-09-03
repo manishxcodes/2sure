877d5e27ff9561cd054402f0214769ac8c44b269
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-09-04 01:05:32 +0530

    chore: integrate useSession in protected route(to check if user if logged in or not)

--- frontend/src/components/ProtectedRoutes.tsx
+++ frontend/src/components/ProtectedRoutes.tsx
@@ -1,10 +1,11 @@
+import { useSession } from '@/context/session-context';
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 
 const ProtectedRoutes: React.FC = () => {
-    const isLoggedIn = false;
+    const { isLoggedIn } = useSession(); 
 
     return isLoggedIn ? <Outlet /> : <Navigate to="/login" />
 }
 
-export default ProtectedRoutes
+export default ProtectedRoutes;
