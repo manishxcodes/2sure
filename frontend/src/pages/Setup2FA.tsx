2d5d176d19d5c2a5a3f62af4623068409a73e1d3
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-09-05 01:56:28 +0530

    chore: add setup page

---
+++ frontend/src/pages/Setup2FA.tsx
@@ -0,0 +1,18 @@
+import { SetupForm } from "@/components/TwoFa/SetupForm";
+import { useNavigate } from "react-router-dom";
+
+const Setup2FA = () => {
+  const navigate = useNavigate();
+
+  const handleSetupComplete = () => {
+    navigate("/verify");
+  }
+
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+        <SetupForm onSetupComplete={handleSetupComplete} />
+    </div>
+  )
+}
+
+export default Setup2FA;
