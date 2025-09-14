5cc826f2d3fee65f4ce035c2e86671f3f23ef7b0
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-09-15 02:08:09 +0530

    add loading state

--- frontend/src/context/session-context.tsx
+++ frontend/src/context/session-context.tsx
@@ -1,33 +1,49 @@
-import type { loginResponse } from "@/schema";
-import { createContext, useContext, useState } from "react";
+import type { loginResponseType } from "@/schema";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export type SessionContextType = {
-    user: loginResponse | null;
-    login: (userData: loginResponse) => void;
+    user: loginResponseType | null;
+    login: (userData: loginResponseType) => void;
     logout: (data: any) => void;
     isLoggedIn: boolean;
+    loading: boolean
 } 
 
 const SessionContext = createContext<SessionContextType | undefined>(undefined);
 
 export const SessionProvider = ({ children }: { children: React.ReactNode }) => {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-    const [user, setUser] = useState<loginResponse | null>(null);
+    const [user, setUser] = useState<loginResponseType | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
 
-    const login = (userData: loginResponse) => {
+    useEffect(() => {
+
+        const storedUser = sessionStorage.getItem("user");
+        if(storedUser) {
+            const parsedUser: loginResponseType = JSON.parse(storedUser);
+            console.log("Useeffect runs: ", parsedUser);
+            setUser(parsedUser);
+            setIsLoggedIn(true);
+        }
+        setLoading(false);
+    }, [])
+
+    const login = (userData: loginResponseType) => {
         setIsLoggedIn(true);
         setUser(userData);
+        sessionStorage.setItem("user", JSON.stringify(userData));
     }
 
     const logout = (data: any) => {
         if(data) {
             setIsLoggedIn(false);
             setUser(null);
+            sessionStorage.removeItem("user");
         }
     }
 
     return (
-        <SessionContext.Provider value={{isLoggedIn, user, login, logout}}>
+        <SessionContext.Provider value={{isLoggedIn, user, login, logout, loading}}>
             {children}
         </SessionContext.Provider>
     )
