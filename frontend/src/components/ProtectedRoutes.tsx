ded1f0aa3f69af7b7f6ed7bd2becc23e5ac86966
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-09-15 02:07:58 +0530

    fix: while refreshing it was taking user to login page. fix by using loading state

--- frontend/src/components/ProtectedRoutes.tsx
+++ frontend/src/components/ProtectedRoutes.tsx
@@ -1,10 +1,15 @@
 import { useSession } from '@/context/session-context';
+import { Loader } from 'lucide-react';
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 
 const ProtectedRoutes: React.FC = () => {
-    const { isLoggedIn } = useSession(); 
-
+    const { isLoggedIn, loading } = useSession(); 
+    if(loading) {
+        return <div className='h-screen w-full flex items-center justify-center'>
+            <Loader className='animate-spin' />
+        </div>
+    }
     return isLoggedIn ? <Outlet /> : <Navigate to="/login" />
 }
 
